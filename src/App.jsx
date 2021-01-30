33fd0de34c59ba9add127716289ab551c2f27f3a
Josef Nagy <josef.nagy@seznam.cz>
2021-01-30 19:03:53 +0100

    fixed some styling so calendar rows are all same height

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,7 @@ import Header from "./components/Header.jsx";
 import Aside from "./components/Aside.jsx";
 import Calendar from "./components/Calendar.jsx";
 
-// import Dropdown from "./components/Dropdown.jsx";
-// import InputBox from "./components/InputBox.jsx";
-
 import history from "./history";
-
-const monthsName = [
-  "Leden",
-  "Únor",
-  "Březen",
-  "Duben",
-  "Květen",
-  "Červen",
-  "Červenec",
-  "Srpen",
-  "Září",
-  "Říjen",
-  "Listopad",
-  "Prosinec",
-];
-
-const options = [
-  {
-    name: "Strojvedoucí",
-    value: "str",
-  },
-  {
-    name: "Staniční dozorce",
-    value: "stD",
-  },
-  {
-    name: "Vedoucí posunu",
-    value: "vp",
-  },
-];
 
 const App = () => {
   return (
@@ -48,15 +15,12 @@ const App = () => {
           <Aside />
           <Header />
           <main className="main">
-            {/* <div className="center">
-          <Dropdown options={options} />
-        </div> */}
-
-            <Calendar />
+            <Route path="/" exact component={Calendar} />
           </main>
         </>
       </Router>
     </div>
   );
 };
+
 export default App;
