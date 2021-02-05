944320fe1c9bf7382001f53b960bae3fbbe3e3a4
Josef Nagy <josef.nagy@seznam.cz>
2021-02-05 12:45:06 +0100

    refactor - navrch statu

---
+++ state.js
@@ -0,0 +1,27 @@
+const state = {
+  events: {
+    allEvents: [
+      {
+        day,
+        month,
+        year,
+        dateId,
+        id,
+        label,
+        function,
+        location,
+        notes,
+        key
+      }, {
+        event 2....
+      }
+    ],
+    selectedDay: {
+      day,
+      month,
+      year,
+      events: [{ event1} ...]
+    },
+    editedDay
+  },
+};
